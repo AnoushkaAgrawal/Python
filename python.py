5b0322280f95b00708ad69a00071570f9a53c6f8
AnoushkaAgrawal <100527622+AnoushkaAgrawal@users.noreply.github.com>
2022-02-27 21:41:09 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 14:25:25 2022
+
+@author: HP
+"""
+
+l1 = [1,4]
+#loops
+for i in range(0,10):
+    print("hi")
+
+def addition(a,b):
+    print(a+b)
+addition(1,9)
+
+def addition(a,b):
+    return(a,b)
+addition(1,9)
+result = addition(1,9)
+
+#websraping
